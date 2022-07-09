6aa9db7d604e8474edb154bb5f5c849236ad8b2c
wangjindong <2214231538@qq.com>
2022-07-09 15:30:19 +0800

    update Test006 file

--- src/com/wang/test/Test006.java
+++ src/com/wang/test/Test006.java
@@ -4,5 +4,6 @@ public class Test006 {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("bbb");
+        System.out.println("master");
     }
 }
