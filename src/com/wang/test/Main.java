ca62ff52f25e3a095552b036ebbe238a97183553
wangjindong <2214231538@qq.com>
2022-07-24 14:33:21 +0800

    update Test006 file

--- src/com/wang/test/Main.java
+++ src/com/wang/test/Main.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("干嘛");
 
         int rootVal = scanner.nextInt();
 
